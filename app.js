3ee11ebc997eb8022b584c8f6fed9faab87d41aa
xaviguasch <xaviguasch@gmail.com>
2019-02-23 17:57:11 +0100

    added CONTACT & ABOUT routes

--- app.js
+++ app.js
@@ -25,6 +25,17 @@ app.get('/', function (req, res) {
     })
 })
 
+app.get('/about', function (req, res) {
+    res.render('about', {
+        aboutContent: aboutContent
+    })
+})
+
+app.get('/contact', function (req, res) {
+    res.render('contact', {
+        contactContent: contactContent
+    })
+})
 
 
 
